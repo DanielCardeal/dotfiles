c4e904caa7ba27356e576a75fff41381ffc31b9f
Daniel Pessoa Cardeal <cardeal.daniel@usp.br>
2022-05-11 12:29:42 -0300

    (nvim): faz telescope buscar em arquivos ocultos
    Do modo como é configurado por padrão, o Telescope ignora todos os
    arquivos ocultos do diretório quando chamando os comandos *find_files* e
    *live_grep*. Esse commit faz com que o Telescope deixe de ignorar esse
    arquivos quando for adequado.

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -40,6 +40,22 @@ require('packer').startup(function()
         },
         config = function()
             local t = require('telescope')
+            t.setup {
+                pickers = {
+                    find_files = {
+                        -- Mostra arquivos ocultos em <leader>ff
+                        hidden = true,
+                        file_ignore_patterns = { '.git' },
+                    },
+                    live_grep = {
+                        -- Busca em arquivos oculto em <leader>sp
+                        additional_args = function(opts)
+                            return {'--hidden'}
+                        end,
+                        file_ignore_patterns = { '.git' },
+                    }
+                }
+            }
             t.load_extension('fzf')
         end
     }
