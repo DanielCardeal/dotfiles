29bf10617f30b66f7e4d9f5ad704c3bf9e34862e
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2022-06-06 14:58:10 -0300

    (fish): faz setup para zoxide (similar a `cd`)

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,8 @@ set -gx EDITOR nvim
 fish_vi_key_bindings
 
 if status is-interactive
+    # Inicializa zoxide (https://github.com/ajeetdsouza/zoxide)
+    zoxide init fish | source
     # Inicializa starship (deve ficar no fim do arquivo)
     starship init fish | source
 end
