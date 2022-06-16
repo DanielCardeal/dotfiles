ecf509f8c156495f310a957e716b14f54d17830c
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2022-06-15 22:25:58 -0300

    (nvim): começa a usar nvim-treesitter-context

--- nvim/.config/nvim/lua/plugin_setup.lua
+++ nvim/.config/nvim/lua/plugin_setup.lua
@@ -155,6 +155,7 @@ require('packer').startup(function()
         'nvim-treesitter/nvim-treesitter-textobjects',
         after = 'nvim-treesitter',
     }
+    use { 'nvim-treesitter/nvim-treesitter-context', after = 'nvim-treesitter' }
 
     -- Trouble
     use {
