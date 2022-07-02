40217f71eb66b8adfc5c80493c21edbb447e66ae
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2022-07-01 21:43:28 -0300

    (nvim,lua): cria mapping para abrir interpretador

--- nvim/.config/nvim/ftplugin/lua.lua
+++ nvim/.config/nvim/ftplugin/lua.lua
@@ -3,6 +3,7 @@ require('nani.utils.ftype_setup').bufEnter("*.lua", function(utils)
         m = {
             name = "lua",
             s = { "<cmd>luafile %<cr>", "source file" },
+            t = { "<cmd>bot 10sp term://lua<cr>", "open interpreter" }
         }
     }, { prefix = '<leader>', buffer = utils.bufnr })
 end)
