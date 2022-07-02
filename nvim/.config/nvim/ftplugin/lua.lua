b1b5d61beddf6cddfbdc68692c4ba3e63363e74c
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2022-07-01 21:42:52 -0300

    (nvim,lua): usa <leader>s como mapping para source file

--- nvim/.config/nvim/ftplugin/lua.lua
+++ nvim/.config/nvim/ftplugin/lua.lua
@@ -2,7 +2,7 @@ require('nani.utils.ftype_setup').bufEnter("*.lua", function(utils)
     utils.wk.register({
         m = {
             name = "lua",
-            S = { "<cmd>luafile %<cr>", "source file" },
+            s = { "<cmd>luafile %<cr>", "source file" },
         }
     }, { prefix = '<leader>', buffer = utils.bufnr })
 end)
