43543f37f1a3f95a0af757e5ceda3a2567d57cd1
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2022-07-05 19:08:28 -0300

    (nvim): remove comentário esquecido em terminal.lua

--- nvim/.config/nvim/lua/nani/terminal.lua
+++ nvim/.config/nvim/lua/nani/terminal.lua
@@ -12,9 +12,8 @@ if vim.fn.executable('nvr') == 1 then
     vim.env.GIT_EDITOR = 'nvr -cc split --remote-wait'
 end
 
--- Delete buffers abertos pelo git automaticamente ao fechar a janela (baseado
+-- Deleta buffers abertos pelo git automaticamente ao fechar a janela (baseado
 -- nas instruções do github do neovim-remote)
--- vim.cmd [[autocmd FileType gitcommit,gitrebase,girconfig set bufhidden=delete]]
 local augroup = vim.api.nvim_create_augroup("NaniTerminal", {})
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "gitcommit,gitrebase,gitconfig",
