cbe1b614b1ae1c453ba1253373d41b04cd65e50c
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2022-10-31 10:41:53 -0300

    (nvim): remove chamada à função deprecada "update_capabilities"

--- nvim/.config/nvim/lua/nani/plugins/lsp.lua
+++ nvim/.config/nvim/lua/nani/plugins/lsp.lua
@@ -22,7 +22,7 @@ function M.config()
     end
 
     -- Aprimora autocomplete dependendo do server
-    local capabilities = require('cmp_nvim_lsp').update_capabilities(
+    local capabilities = require('cmp_nvim_lsp').default_capabilities(
         vim.lsp.protocol.make_client_capabilities()
     )
 
