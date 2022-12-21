e73e33fd071f2c90dedd1dce329aaecb3618e4a6
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2022-12-21 02:07:26 -0300

    nvim,tex: limita casos onde a janela de quickfix é aberta

--- nvim/.config/nvim/ftplugin/tex.lua
+++ nvim/.config/nvim/ftplugin/tex.lua
@@ -3,6 +3,7 @@
 -- #####################
 vim.g.vimtex_view_general_viewer = 'evince'
 vim.g.vimtex_complete_enabled = false
+vim.g.vimtex_quickfix_mode = 2
 
 -- Correção de texto automaticamente acionada no LaTeX
 vim.opt_local.spell = true
