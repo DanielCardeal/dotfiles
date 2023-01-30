767305277d5925c5d80487bf40f10ab467535d94
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2023-01-30 18:28:20 -0300

    nvim: cria alguns snippets para pythnon e LaTeX

--- nvim/.config/nvim/ftplugin/python.lua
+++ nvim/.config/nvim/ftplugin/python.lua
@@ -2,6 +2,7 @@
 local ls = require('luasnip')
 local s = ls.snippet
 local t = ls.text_node
+local i = ls.insert_node
 local c = ls.choice_node
 local fmt = require('luasnip.extras.fmt').fmt
 
@@ -9,5 +10,19 @@ ls.add_snippets("python", {
     s("#!", fmt([[
     #! /bin/env {}
 
-    ]], { c(1, { t('python'), t('python3'), t('python2') }) }))
+    ]], { c(1, { t('python'), t('python3'), t('python2') }) })),
+
+    s("ifmain", fmt([[
+    if __name__ == "__main__":
+        {}
+    ]], { i(1, "pass") })),
+
+    s('class', fmt([[
+    class {}:
+        {}
+    ]], { i(1, "Name"), i(2, "pass") })),
+
+    -- Imports
+    s('pd', t("import pandas as pd")),
+    s('np', t("import numpy as np")),
 })
--- nvim/.config/nvim/ftplugin/tex.lua
+++ nvim/.config/nvim/ftplugin/tex.lua
@@ -7,3 +7,57 @@ vim.g.vimtex_quickfix_mode = 2
 
 -- Correção de texto automaticamente acionada no LaTeX
 vim.opt_local.spell = true
+
+-- ##############
+--    SNIPPETS
+-- ##############
+local ls = require('luasnip')
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local fmt = require('luasnip.extras.fmt').fmt
+local rep = require('luasnip.extras').rep
+
+ls.add_snippets('tex', {
+    -- #############
+    --    SIMPLES
+    -- #############
+    -- Quote
+    s("q", fmt("\\quote{{{}}} ", { i(1) })),
+
+    -- Itálico
+    s("i", fmt("\\textit{{{}}} ", { i(1) })),
+
+    -- Negrito
+    s("b", fmt("\\textbf{{{}}} ", { i(1) })),
+
+    -- Matemática
+    s("m", fmt("${}$", { i(1) })),
+
+    -- ##################
+    --    ENVIRONMENTS
+    -- ##################
+    -- Básico
+    s("beg", fmt([[
+    \begin{{{}}}
+        {}
+    \end{{{}}}
+    ]], {
+        i(1, 'environment'),
+        i(0),
+        rep(1)
+    })),
+
+    -- Imagem
+    s("img", fmt([[
+    \begin{{figure}}[ht!]
+        \includegraphics{{{}}}
+        \caption{{{}}}
+    \end{{figure}}
+
+    ]], {
+        i(1, 'image path'),
+        i(2, 'image caption')
+    })),
+})
