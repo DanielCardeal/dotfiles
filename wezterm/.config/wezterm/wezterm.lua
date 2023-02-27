72f82a268f1dde2465a4a9f4ae8f5348d4ff34d6
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2023-02-27 12:05:39 -0300

    wez: adiciona configurações de janela

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,6 +1,14 @@
 -- local wezterm = require 'wezterm'
 
 return {
-    window_background_opacity = 0.90,
     color_scheme = 'Gruvbox Dark',
+    font_size = 10,
+
+    window_decorations = 'NONE',
+    window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0,
+    },
 }
