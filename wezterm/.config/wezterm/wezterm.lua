169e89b0774b71f5a5a9f396bd642850673dab82
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2023-02-28 12:01:19 -0300

    wez: adiciona mais configurações

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,9 +1,19 @@
--- local wezterm = require 'wezterm'
+local wezterm = require 'wezterm'
 
 return {
-    color_scheme = 'Gruvbox Dark',
-    font_size = 10,
-
+    -- ###########
+    --    STYLE
+    -- ###########
+    window_background_opacity = 0.95,
+    color_scheme = "Gruvbox dark, hard (base16)",
+    font_size = 13,
+    colors = {
+        cursor_bg = "fe8019", -- laranja gruvbox
+    },
+    -- ###################
+    --    WINDOW LAYOUT
+    -- ###################
+    hide_tab_bar_if_only_one_tab = true,
     window_decorations = 'NONE',
     window_padding = {
         left = 0,
@@ -11,4 +21,18 @@ return {
         top = 0,
         bottom = 0,
     },
+    -- #############
+    --    KEYMAPS
+    -- #############
+    leader = { key = 'h', mods = 'CTRL', timeout_milliseconds = 1000 },
+    keys = {
+        -- SPLITS
+        { key = 'v',          mods = 'LEADER',     action = wezterm.action.SplitHorizontal },
+        { key = 's',          mods = 'LEADER',     action = wezterm.action.SplitVertical },
+        -- MOVING
+        { key = 'h',          mods = 'LEADER',     action = wezterm.action.PaneSelect },
+        -- TABS
+        { key = 'RightArrow', mods = 'CTRL|SHIFT', action = wezterm.action.ActivateTabRelative(1) },
+        { key = 'LeftArrow',  mods = 'CTRL|SHIFT', action = wezterm.action.ActivateTabRelative( -1) },
+    }
 }
