5445b6c0952963fa97362bca5ca3fd8c8380ce56
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2023-03-21 22:06:49 -0300

    nvim,c: remove clang_format desnecessário

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -519,8 +519,6 @@ local null_ls = require('null-ls')
 null_ls.setup {
     debug = true,
     sources = {
-        -- C
-        null_ls.builtins.formatting.clang_format,
         -- Python
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
