96f702fba7ef3d9b6611a431c1fdec34999c90d4
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2023-05-23 20:12:56 -0300

    ipy: cria módulo de configuração do ipython

---
+++ ipython/.ipython/profile_default/ipython_config.py
@@ -0,0 +1,8 @@
+c.TerminalInteractiveShell.editing_mode = 'vi'
+
+# Importa bibliotecas padrão
+c.InteractiveShellApp.exec_lines = [
+    'import numpy as np',
+    'import pandas as pd',
+    'import matplotlib.pyplot as plt'
+]
