39fe0793ffe9580a3b967d91edc02b275841aa1f
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2023-05-24 16:50:42 -0300

    ipy: remove delay de sair do vi mode ao apertar esc

--- ipython/.ipython/profile_default/ipython_config.py
+++ ipython/.ipython/profile_default/ipython_config.py
@@ -1,8 +1,9 @@
-c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
+c.TerminalInteractiveShell.editing_mode = "vi"
 
 # Importa bibliotecas padrão
 c.InteractiveShellApp.exec_lines = [
-    'import numpy as np',
-    'import pandas as pd',
-    'import matplotlib.pyplot as plt'
+    "import numpy as np",
+    "import pandas as pd",
+    "import matplotlib.pyplot as plt",
 ]
