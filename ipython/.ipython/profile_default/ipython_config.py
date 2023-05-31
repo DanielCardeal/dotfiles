3381b6d3582b7a8ec12ad128b5cc180eecdaa1d8
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2023-05-31 19:04:47 -0300

    ipy: melhora desenvolvimento iterativo com autoreload

--- ipython/.ipython/profile_default/ipython_config.py
+++ ipython/.ipython/profile_default/ipython_config.py
@@ -1,8 +1,12 @@
 c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
 c.TerminalInteractiveShell.editing_mode = "vi"
 
+# Extensões carregadas por padrão
+c.InteractiveShellApp.extensions = ["autoreload"]
+
 # Importa bibliotecas padrão
 c.InteractiveShellApp.exec_lines = [
+    "%autoreload 2",
     "import numpy as np",
     "import pandas as pd",
     "import matplotlib.pyplot as plt",
