40c97b26ff9e3d0c9cea40fc790a6455614eafd7
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2023-07-04 12:37:59 -0300

    ipy: melhora integração com matplotlib

--- ipython/.ipython/profile_default/ipython_config.py
+++ ipython/.ipython/profile_default/ipython_config.py
@@ -1,5 +1,12 @@
+c.TerminalInteractiveShell.autoindent = True
+c.TerminalInteractiveShell.colors = 'Linux'
 c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
 c.TerminalInteractiveShell.editing_mode = "vi"
+
+c.TerminalIPythonApp.display_banner = False
+
+# Carrega numpy e matplotlib automaticamente
+c.InteractiveShellApp.pylab = 'auto'
 
 # Extensões carregadas por padrão
 c.InteractiveShellApp.extensions = ["autoreload"]
@@ -7,7 +14,4 @@ c.InteractiveShellApp.extensions = ["autoreload"]
 # Importa bibliotecas padrão
 c.InteractiveShellApp.exec_lines = [
     "%autoreload 2",
-    "import numpy as np",
-    "import pandas as pd",
-    "import matplotlib.pyplot as plt",
 ]
