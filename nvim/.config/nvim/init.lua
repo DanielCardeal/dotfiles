8904be1e3f319f1b68c5bd1dcd6c802e97bec3cf
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2025-06-23 13:20:29 -0300

    nvim: usa lualine ao invés de mini.statusline

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -129,8 +129,6 @@ require("lazy").setup({
             require("mini.move").setup()
             -- File tree
             require("mini.files").setup()
-            -- Statusline
-            require("mini.statusline").setup()
         end,
     },
 
@@ -220,6 +218,61 @@ require("lazy").setup({
                 end,
             })
         end,
+    },
+
+    -- ################
+    --    StatusLine
+    -- ################
+    {
+        "nvim-lualine/lualine.nvim",
+        opts = {
+            options = {
+                section_separators = { left = "", right = "" },
+                component_separators = { left = "", right = "" },
+            },
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = {
+                    {
+                        "filename",
+                        path = 1,
+                    }
+                },
+                lualine_c = {
+                    "branch",
+                    "diff",
+                },
+                lualine_x = {
+                    {
+                        "diagnostics",
+                        symbols = {
+                            error = " ",
+                            warn = " ",
+                            info = " ",
+                            hint = " ",
+                        },
+                    },
+                },
+                lualine_y = { "filetype" },
+                lualine_z = { "location" },
+            },
+            winbar = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { "filename" },
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {},
+            },
+            inactive_winbar = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { "filename" },
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {},
+            },
+        },
     },
 
     -- ################
